c460a33dc8381c2acab480060d4b7651856a8a0c
Rafael Lima Barreto <rafaellbecom@gmail.com>
2024-02-17 14:34:39 -0300

    Reorganizando código de filtragem

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,26 @@ btnsFiltros.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponiveis' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponiveis' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirLivros(livrosFiltrados)
+
+    if(categoria == 'disponiveis') {
+        exibirValorTotalDeLivroDisponiveis()
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDeLivroDisponiveis() {
+    sectionValorTotalDeLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">299,99</span></p>
+        </div>
+    `
 }
